adf3d984687211e94292b903688dc83820c52dd4
Andrew Lorente <andrew.lorente@gmail.com>
2012-02-02 22:08:55 -0800

    The cloud is the MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOON

--- app/controllers/wanker_controller.rb
+++ app/controllers/wanker_controller.rb
@@ -4,6 +4,10 @@ class WankerController < ApplicationController
     agent = Mechanize.new
     page = agent.get "http://news.ycombinator.com#{ request.fullpath }", :encoding => 'UTF-8'
     html_text = page.body
+    html_text = html_text.gsub(/Cloud/, 'Moon')
+    html_text = html_text.gsub(/CLOUD/, 'MOON')
+    html_text = html_text.gsub(/cloud/i, 'moon')
+
     html_text = html_text.gsub(/Hack/, 'Wank')
     html_text = html_text.gsub(/HACK/, 'WANK')
     html_text = html_text.gsub(/hack/i, 'wank')

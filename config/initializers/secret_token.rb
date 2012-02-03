7932faa7d688648af072acecbc97c474e5df035b
Andrew Lorente <andrew.lorente@gmail.com>
2012-02-02 18:34:50 -0800

    rails new .  --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wankernews::Application.config.secret_token = '4a221aa709fda8f1bfd3cd94612248e489a7ed7609493a673cd0d26101cdbf09ab89a4a59c2108d678b6f2a53554eeaeebeb135c7940fa0ef91ecb89e40a3362'

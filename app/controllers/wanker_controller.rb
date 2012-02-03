3549983b3f6c315adbe6de18be5d6dda0f85ce5c
Andrew Lorente <andrew.lorente@gmail.com>
2012-02-02 20:02:30 -0800

    Replace 'hack' with 'wank'
    yessssss
    
    Geez I'd rather do a case-retaining replace but I got tired of googling
    for how. It's probably possible!

--- app/controllers/wanker_controller.rb
+++ app/controllers/wanker_controller.rb
@@ -3,7 +3,11 @@ class WankerController < ApplicationController
   def wank
     @foo= ''
     open('http://news.ycombinator.com') do |f|
-      f.each_line {|line| @foo += line }
+      f.each_line do |line|
+        line = line.gsub(/Hack/, 'Wank')
+        line = line.gsub(/hack/i, 'wank')
+        @foo += line
+      end
     end
     render :inline => @foo
   end

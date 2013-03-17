6351dfc20592089d8f6a941e63be5de1c6ddec2a
Andrew Lorente <andrew.lorente@gmail.com>
2013-03-17 08:07:35 -0700

    s dot gif

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Wankernews::Application.routes.draw do
   match '/newslogin/', :to => 'login#login'
   match '/newslogin/*whatever', :to => 'login#login'
 
+  match '/s.gif', :to => "wanker#passthrough", :format => false
   match '/grayarrow.gif', :to => "wanker#passthrough", :format => false
   match '/y18.gif', :to => "wanker#passthrough", :format => false
   match '/*wankernews_path', :to => "wanker#wank", :format => false
